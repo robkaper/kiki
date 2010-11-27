53dd53f13df72eb1e8772bded7bbce6c69ddb056
Rob Kaper <rjkaper@capsi.com>
2010-11-27 13:58:16 +0100

    add facebook xd to site base

---
+++ htdocs/facebook-xd/index.php
@@ -0,0 +1,43 @@
+<?
+  include "../../lib/init.php";
+?>
+<!doctype html>
+<html xmlns:fb="http://www.facebook.com/2008/fbml">
+  <head>
+    <title>Facebook canvas</title>
+  </head>
+  <body>
+
+<p>
+Deze applicatie koppelt Facebook accounts aan website <a target="_top" href="http://<?= $SERVER_['SERVER_NAME ] ?>"><?= Config::$siteName ?></a>, meer niet.</p>
+
+    <!--
+      We use the JS SDK to provide a richer user experience. For more info,
+      look here: http://github.com/facebook/connect-js
+    -->
+    <div id="fb-root"></div>
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId   : '<?php echo $fb->getAppId(); ?>',
+          session : <?php echo json_encode($session); ?>, // don't refetch the session when PHP already has it
+          status  : true, // check login status
+          cookie  : true, // enable cookies to allow the server to access the session
+          xfbml   : true // parse XFBML
+        });
+
+        // whenever the user logs in, we refresh the page
+        FB.Event.subscribe('auth.login', function() {
+          window.location.reload();
+        });
+      };
+
+      (function() {
+        var e = document.createElement('script');
+        e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+        e.async = true;
+        document.getElementById('fb-root').appendChild(e);
+      }());
+    </script>
+  </body>
+</html>
