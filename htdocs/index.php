d71ba33399be15365a6378fc5d43ce2ce475a9fd
Rob Kaper <rjkaper@capsi.com>
2011-04-04 11:12:43 +0200

    installation requirements checkpage

---
+++ htdocs/index.php
@@ -0,0 +1,24 @@
+<?
+  include_once "../lib/init.php";
+
+  $page = new Page( "Kiki CMS Status" );
+  $page->header();
+
+  echo "<h2>PHP modules and PEAR/PECL extensions</h2>\n";
+
+  $extensions = array( 'curl' => null, 'mysql' => null , 'Mailparse (PECL)' => 'mailparse_msg_create' );
+
+  echo "<ul>\n";
+  foreach( $extensions as $extension => $function )
+  {
+    if ( $function )
+      $loaded = function_exists($function);
+    else
+      $loaded = extension_loaded( $module );
+    $loadedStr = $loaded ? "enabled" : "<span style=\"color: red\">disabled</span>";
+    echo "<li><strong>$module</strong>: ${loadedStr}.</li>\n";
+  }
+  echo "</ul>\n";
+
+  $page->footer();
+?>
