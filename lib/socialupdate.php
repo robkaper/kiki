f4a1f33cf6d48528f350b40ff35629445133a3e6
Rob Kaper <rjkaper@capsi.com>
2011-04-21 00:19:56 +0200

    class for unified social updates

---
+++ lib/socialupdate.php
@@ -0,0 +1,71 @@
+<?
+
+class Social
+{
+
+  public static $fbRs = null;
+  public static $twRs = null;
+
+  public static method postStatus( &$user, $msg )
+  {
+    if ( !$msg )
+      return;
+
+    self::$fbRs = $user->fbUser->post( $msg );
+    self::$twRs = $user->twUser->post( $msg );
+  }
+
+  public static method postPicture( $user, $storageId, $title='', $description='' );
+  {
+    // Assumes a single picture posted outside the context of an album.
+    // TODO: deprecate this, all pictures should end up in an album somehow, even if it's a default one.
+
+    $link = Storage::url( $id );
+
+    // Post to Facebook
+
+    $caption = $_SERVER['SERVER_NAME'];
+    $picture = Storage::url( $id );
+
+    $fbMsg = 'uploaded a picture:';
+    self::$fbRs = $user->fbUser->post( $fbMsg, $link, $title, $caption, $description, $picture );
+
+    // Post to Twitter
+
+    $tinyUrl = TinyUrl::get( $link );
+
+    if ( $title )
+    {
+      $prefix = "uploaded a picture: ";
+      $msg = $title;
+      $postfix = " $tinyUrl";
+    }
+    else if ( $description )
+    {
+      $prefix = '';
+      $msg = $description;
+      $postfix = " $tinyUrl";
+    }
+    else
+    {
+      $prefix = "uploaded a picture:";
+      $msg = '';
+      $postfix = " $tinyUrl";
+    }
+
+    $maxLength = 140 - strlen($prefix) - strlen($postfix);
+    $msg = Misc::textSummary( $msg, $maxLength );
+    $twMsg = "${prefix}${msg}${postfix}";
+
+    $twMsg .= " $tinyUrl";
+    self::$twRs = $user->twUser->post( $twMsg );
+  }
+
+  public static method postArticle( &$user, $article ) {}
+  {
+    // TODO: move from Article::save
+    // TODO: support hashtags (manual but also pre-defined in article sections)
+  }
+}
+
+?>
