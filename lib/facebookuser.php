84dac2d0b24c425619d4eee5235f4e9e69d0036b
Rob Kaper <rjkaper@capsi.com>
2011-09-12 22:44:18 +0200

    createEvent

--- lib/facebookuser.php
+++ lib/facebookuser.php
@@ -231,6 +231,53 @@ class FacebookUser
     return $result;
   }
 
+  public function createEvent( $title, $start, $end, $location, $description )
+  {
+    if ( !$this->authenticated || !$this->fb )
+    {
+      echo "not authenticated\n";
+      return false;
+    }
+
+
+    // @fixme times must be EDT (Facebook time)
+    // Privacy types: OPEN, CLOSED, SECRET
+    $event_info = array(
+      "privacy_type" => "OPEN",
+      "name" => "Facebook/CMS koppeling Testje",
+      "host" => "Me",
+      "start_time" => $start,
+      "end_time" => $end,
+      "location" => $location,
+      "description" => $description
+    );
+
+    return;
+    
+    // @todo add photo and invite support
+
+    //Path to photo (only tested with relative path to same directory)
+    // $file = "end300.jpg";
+    // The key part - The path to the file with the CURL syntax
+    // $event_info[basename($file)] = '@' . realpath($file);
+
+    // $facebook->setFileUploadSupport(true);
+    // $attachment = array( 'message' => $caption );    
+    // $attachment[basename($file_path)] = '@' . realpath($file_path);
+    // $result = $facebook->api('me/photos','post',$attachment);
+
+    // print_r( $event_info );
+    // var_dump($this->fb->api('me/events','post',$event_info));
+    // var_dump($facebook->api("$pageId/events", 'post', $event_info));
+
+    // $fb->api( array(
+    //   'method' => 'events.invite',
+    //   'eid' => $event_id,
+    //   'uids' => $id_array,
+    //   'personal_message' => $message,
+    // ) );
+  }
+
   public function storePerm( $perm )
   {
     $qPerm = $this->db->escape( $perm );
