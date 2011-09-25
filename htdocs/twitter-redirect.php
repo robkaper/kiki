dd3966fa50980812dd61dc9e581592fd60b57102
Rob Kaper <rjkaper@capsi.com>
2011-09-25 21:44:18 +0200

    remove callback url, callback is referer

--- htdocs/twitter-redirect.php
+++ htdocs/twitter-redirect.php
@@ -9,22 +9,22 @@
 */
 
 include_once "../lib/init.php";
-session_start();
 
-require_once( $GLOBALS['kiki']. '/lib/twitteroauth/twitteroauth.php' );
+session_start();
 
 // Build TwitterOAuth object with client credentials.
 $connection = new TwitterOAuth( Config::$twitterApp, Config::$twitterSecret );
  
 // Get temporary credentials.
-$request_token = $connection->getRequestToken( Config::$twitterCallback );
+$request_token = $connection->getRequestToken( $_SERVER['HTTP_REFERER'] );
 
 // Save temporary credentials to session.
 $_SESSION['oauth_token'] = $token = $request_token['oauth_token'];
 $_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
  
 // If last connection failed don't display authorization link.
-switch ($connection->http_code) {
+switch ($connection->http_code)
+{
   case 200:
     // Build authorize URL and redirect user to Twitter.
     $url = $connection->getAuthorizeURL($token);
