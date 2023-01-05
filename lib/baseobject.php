b631c6081eb3e02a5390933edbe4225cb4684928
Rob Kaper <rob@robkaper.nl>
2023-01-05 01:26:13 +0100

    prefer underscores for variables, more consistent with db naming, other small updates

--- lib/baseobject.php
+++ lib/baseobject.php
@@ -22,27 +22,28 @@ abstract class BaseObject
   protected $type = null;
   
   protected $id = 0;
-  protected $objectId = 0;
+  protected $object_id = 0;
 
-  protected $title = null;
-  protected $uriAlias = null;
-  
   protected $ctime = null;
   protected $mtime = null;
 
-	protected $visible = false;
-	protected $userId = 0;
-	protected $sectionId = 0;
+  protected $name = null;
+//  protected $uriAlias = null;
+  
 
-  protected $publications;
+//  protected $visible = false;
+//  protected $userId = 0;
+//  protected $sectionId = 0;
 
-  public function __construct( $id = 0, $objectId = 0 )
+//  protected $publications;
+
+  public function __construct( $id = 0, $object_id = 0 )
   {
     $this->db = Core::getDb();
 
     $this->id = $id;
-    $this->objectId = $objectId;
-    if ( $this->id || $this->objectId )
+    $this->object_id = $object_id;
+    if ( $this->id || $this->object_id )
       $this->load();
     else
       $this->reset();
@@ -51,17 +52,19 @@ abstract class BaseObject
   public function reset()
   {
     $this->id = 0;
-    $this->objectId = 0;
-
-    $this->title = null;
-    $this->uriAlias = null;
-
+    $this->object_id = 0;
     $this->ctime = null;
     $this->mtime = null;
 
-		$this->visible = false;
-		$this->userId = 0;
-		$this->sectionId = 0;
+    $this->name = null;
+    
+    return;
+
+    $this->uriAlias = null;
+
+    $this->visible = false;
+    $this->userId = 0;
+    $this->sectionId = 0;
 
     $this->publications = null;
   }
@@ -74,30 +77,38 @@ abstract class BaseObject
       return;
 
     $this->id = $o->id;
-    $this->objectId = $o->object_id;
+    $this->object_id = $o->object_id;
 
     // $this->title = $o->title;
     // $this->uriAlias = $o->uriAlias;
 
     $this->ctime = $o->ctime;
     $this->mtime = $o->mtime;
+    
+    $this->name = $o->name ?? null;
+    
+    return;
 
-		// FIXME: issets are necessary because not all Object child classes have these set in their queries e.g. load(), i.e. User.
-		$this->visible = isset($o->visible) ? $o->visible : false;
-		$this->userId = isset($o->user_id) ? $o->user_id : 0;
-		$this->sectionId = isset($o->section_id) ? $o->section_id : 0;
+    // FIXME: issets are necessary because not all Object child classes have these set in their queries e.g. load(), i.e. User.
+    $this->visible = isset($o->visible) ? $o->visible : false;
+    $this->userId = isset($o->user_id) ? $o->user_id : 0;
+    $this->sectionId = isset($o->section_id) ? $o->section_id : 0;
   }
 
   final public function save()
   {
-    if ( !$this->objectId )
+    if ( !$this->object_id )
     {
       $qCtime = (isset($this->ctime) && is_numeric($this->ctime) && $this->ctime) ? sprintf( "'%s'", date("Y-m-d H:i:s", $this->ctime) ) : "now()";
 
-      $q = $this->db->buildQuery( "INSERT INTO objects (type,ctime,mtime,visible,user_id,section_id) values('%s',$qCtime,now(),%d,%d,%d)", get_class($this), $this->visible, $this->userId, $this->sectionId );
+//      $q = $this->db->buildQuery( "INSERT INTO objects (type,ctime,mtime,visible,user_id,section_id) values('%s',$qCtime,now(),%d,%d,%d)", get_class($this), $this->visible, $this->userId, $this->sectionId );
+      $q = "INSERT INTO objects (`name`) values('%s')";
+      $q = $this->db->buildQuery( $q, $this->name );
+//      echo $q. PHP_EOL;
+      Log::debug( $q );
       $rs = $this->db->query($q);
       if ( $rs )
-        $this->objectId = $this->db->lastInsertId($rs);
+        $this->object_id = $this->db->lastInsertId($rs);
     }
 
     $this->id ? $this->dbUpdate() : $this->dbInsert();
@@ -105,21 +116,34 @@ abstract class BaseObject
 
   protected function dbUpdate()
   {
+/*
     $q = $this->db->buildQuery(
       "UPDATE objects SET ctime='%s', mtime=now(), visible=%d, user_id=%d, section_id=%d WHERE object_id=%d",
-      $this->ctime, $this->visible, $this->userId, $this->sectionId, $this->objectId
+      $this->ctime, $this->visible, $this->userId, $this->sectionId, $this->object_id
     );
+*/
+
+    $q = $this->db->buildQuery(
+      "UPDATE objects SET name='%s' WHERE object_id=%d",
+      $this->name, $this->object_id
+    );
+
+    Log::debug( $q );
 
     $this->db->query($q);
   }
 
-  final public function type() { return get_class($this); }
   final public function setId( $id ) { $this->id = $id; }
   final public function id() { return $this->id; }
-  final public function setObjectId( $objectId ) { $this->objectId = $objectId; }
-  final public function objectId() { return $this->objectId; }
+  final public function setObjectId( $object_id ) { $this->object_id = $object_id; }
+  final public function objectId() { return $this->object_id; }
   final public function setCtime( $ctime ) { $this->ctime = $ctime; }
   final public function ctime() { return $this->ctime; }
+
+  final public function setName( $name ) { $this->name = $name; }
+  public function name() { return $this->name; }
+
+  final public function type() { return get_class($this); }
 
   final public function setVisible( $visible ) { $this->visible = $visible; }
   final public function visible() { return $this->visible; }
@@ -132,7 +156,7 @@ abstract class BaseObject
   {
     $this->publications = array();
 
-    $q = $this->db->buildQuery( "SELECT publication_id, p.connection_id, p.external_id, c.service FROM publications p LEFT join connections c ON c.external_id=p.connection_id WHERE p.object_id=%d", $this->objectId );
+    $q = $this->db->buildQuery( "SELECT publication_id, p.connection_id, p.external_id, c.service FROM publications p LEFT join connections c ON c.external_id=p.connection_id WHERE p.object_id=%d", $this->object_id );
     $rs = $this->db->query($q);
     while( $o = $this->db->fetchObject($rs) )
     {
@@ -145,7 +169,7 @@ abstract class BaseObject
   // FIXME: should not directly return templateData, but Like objects (which have a templatData method)
   final public function likes()
   {
-    $q = $this->db->buildQuery( "SELECT external_id AS id FROM likes LEFT JOIN connections ON likes.user_connection_id=connections.id WHERE object_id=%d", $this->objectId );
+    $q = $this->db->buildQuery( "SELECT external_id AS id FROM likes LEFT JOIN connections ON likes.user_connection_id=connections.id WHERE object_id=%d", $this->object_id );
     $likeUsers = $this->db->getObjectIds($q);
 
     $likes = array();
