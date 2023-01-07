3698d76c25912f01746345d007b2d9f9b2e0bc70
Rob Kaper <rob@robkaper.nl>
2023-01-07 01:44:51 +0100

    proper true/false returns for main controller

--- lib/controller/thumbnails.php
+++ lib/controller/thumbnails.php
@@ -2,6 +2,8 @@
 
 namespace Kiki\Controller;
 
+use Kiki\Log;
+
 class Thumbnails extends \Kiki\Controller
 {
   public function exec()
@@ -9,21 +11,23 @@ class Thumbnails extends \Kiki\Controller
     list( $dummy, $id, $w, $h, $dummy, $crop ) = $this->objectId;
 
     $this->objectId = $id;
-    
+
     if ( !$fileName = \Kiki\Storage::localFile($this->objectId) )
-      return;
+      return false;
 
     if ( !file_exists($fileName) )
-      return;
+      return false;
 
     $scaleFile = \Kiki\Storage::generateThumb( $fileName, $w, $h, $crop );
     if ( !file_exists($scaleFile) )
-      return;
+      return false;
 
     $this->altContentType = \Kiki\Storage::getMimeType( \Kiki\Storage::getExtension($scaleFile) );
     $this->template = null; // Send content directly, without a template.
     $this->status = 200;
     $this->content = file_get_contents($scaleFile);
+    
+    return true;
   }
 }
 
