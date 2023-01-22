bedb6f61d28866bf3fd18282eaf96c238db28e9b
Rob Kaper <rob@robkaper.nl>
2023-01-22 20:09:12 +0100

    also inclode @todo etc comments

--- find-issues.sh
+++ find-issues.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-egrep -riI "// (FIXME|TODO|DEBUG)" htdocs/ lib/ bin/ 2>&1 | egrep -v "(^htdocs/vendor)"
+egrep -riI "(// |@)(FIXME|TODO|DEBUG)" htdocs/ lib/ bin/ 2>&1 | egrep -v "(^htdocs/vendor)"
 
 egrep -riI "(\$debug|em-|(<style=)|<script>)" htdocs/ lib/ bin/ 2>&1 | egrep -v "(^htdocs/(vendor|images))"
 
