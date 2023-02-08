f338a0ea5f8e99fbaf42fba5fad4119acc506797
Rob Kaper <rob@robkaper.nl>
2023-02-08 17:24:41 +0100

    remove legacy, use Kiki\Log

--- lib/connectionservice/factory.php
+++ lib/connectionservice/factory.php
@@ -2,20 +2,19 @@
 
 namespace Kiki\ConnectionService;
 
+use Kiki\Log;
+
 class Factory
 {
   static public function getInstance( $service )
   {
-		// TODO: remove migration from namespaces
-		$service = str_replace("Connection_", "", $service);
-
     $class = ucfirst($service);
-		if ( !strstr($class, __NAMESPACE__) )
-			$class = __NAMESPACE__. "\\". $class;
+    if ( !strstr($class, __NAMESPACE__) )
+      $class = __NAMESPACE__. "\\". $class;
 
     if ( !class_exists($class) )
     {
-      \Kiki\Log::error( "Non-existant class $class requested" );
+      Log::error( "Non-existant class $class requested" );
       return null;
     }
 
