6b08592491413de339f722691f3cbea71adbe7ed
Rob Kaper <rob@robkaper.nl>
2023-02-17 18:41:45 +0100

    add linenumbers

--- find-issues.sh
+++ find-issues.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-egrep -riI "(// |@)(FIXME|TODO|DEBUG)" htdocs/ lib/ bin/ 2>&1 | egrep -v "(^htdocs/vendor)"
+egrep -rinI "(// |@)(FIXME|TODO|DEBUG)" htdocs/ lib/ bin/ 2>&1 | egrep -v "(^htdocs/vendor)"
 
-egrep -riI "(\$debug|em-|(<style=)|<script>)" htdocs/ lib/ bin/ 2>&1 | egrep -v "(^htdocs/(vendor|images))"
-
+egrep -rinI "(\$debug|em-|(<style=)|<script>)" htdocs/ lib/ bin/ 2>&1 | egrep -v "(^htdocs/(vendor|images))"
