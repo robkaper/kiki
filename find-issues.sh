95469432e71c16cdb39f8fae5d0531df2f24a630
Rob Kaper <rob@robkaper.nl>
2023-08-04 22:09:50 +0200

    no more need for deprecated tag grep

--- find-issues.sh
+++ find-issues.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
 egrep -rinI "(// |@)(FIXME|TODO|DEBUG)" htdocs/ lib/ bin/ 2>&1 | egrep -v "(^htdocs/vendor)"
-
-egrep -rinI "(\$debug|em-|(<style=)|<script>)" htdocs/ lib/ bin/ 2>&1 | egrep -v "(^htdocs/(vendor|images))"
