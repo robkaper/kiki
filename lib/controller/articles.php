15905d3660c75f888ea48760e8288d644e8f5c73
Rob Kaper <rob@robkaper.nl>
2024-01-17 22:45:27 +0100

    refactor detailAction

--- lib/controller/articles.php
+++ lib/controller/articles.php
@@ -18,11 +18,7 @@ class Articles extends \Kiki\Controller
   public function actionHandler()
   {
     if ( $this->action )
-    {
-      // TODO: refactor into detailAction();
-      $this->objectId = $this->action ?? null;
-      return $this->indexAction();
-    }
+      return $this->detailAction();
 
     return $this->indexAction();
   }
@@ -34,17 +30,14 @@ class Articles extends \Kiki\Controller
 
     $this->initTemplateData();
 
-    $this->objectId = $this->action ?? null;
-
     $db = Core::getDb();
     $user = Core::getUser();
 
-    $template = Template::getInstance();
-
     $this->instanceId = Section::getIdFromBaseUri($this->context);
 
-    ob_start();
+    $template = Template::getInstance();
 
+/*
     $q = $db->buildQuery( "SELECT id FROM articles a LEFT JOIN objects o ON o.object_id=a.object_id WHERE a.section_id=%d AND ((a.visible=1 AND o.ctime<=now()) OR o.user_id=%d) ORDER BY o.ctime DESC LIMIT 10", $this->instanceId, $user->id() );
 
     $articleIds = $db->getObjectIds($q);
@@ -61,118 +54,133 @@ class Articles extends \Kiki\Controller
       $this->objectId = null;
       $currentPage = $matches[1];
     }
+*/
 
-    if ( isset($this->objectId) && $this->objectId )
+    $section = new \Kiki\Section( $this->instanceId );
+
+    $itemsPerPage = 25;
+    if ( !isset($currentPage) )
+      $currentPage = 1;
+
+    $this->status = 200;
+    $this->title = $section->title();
+    $this->template = 'pages/articles/index';
+
+    $this->content = null; // MultiBanner::articles( $section->id() );
+
+    $article = new Article();
+
+    $q = $db->buildQuery( "SELECT count(*)
+      FROM articles a, objects o
+      WHERE o.object_id=a.object_id
+      AND type IN ('%s', '%s')
+        AND a.section_id=%d AND ((a.visible=1 AND ctime<=now()) OR user_id=%d)",
+      'Article', 'Kiki\Article',
+      $this->instanceId,
+      $user->id()
+    );
+    $totalPosts = $db->getSingleValue($q);
+
+    $paging = new \Kiki\Paging();
+    $paging->setCurrentPage( $currentPage );
+    $paging->setItemsPerPage( $itemsPerPage );
+    $paging->setTotalItems( $totalPosts );
+
+    $q = $db->buildQuery( "SELECT o.object_id, ctime, type
+      FROM articles a, objects o
+      WHERE o.object_id=a.object_id
+      AND type IN ('%s', '%s')
+      AND section_id=%d AND ( (a.visible=1 AND ctime<=now()) OR user_id=%d)
+      ORDER BY ctime DESC
+      LIMIT %d,%d",
+      'Article',
+      'Kiki\Article',
+      $this->instanceId,
+      $user->id(),
+      $paging->firstItem()-1,
+      $itemsPerPage
+    );
+    $articles = $db->getObjects($q);
+
+    foreach( $articles as $dbArticle )
     {
-      // FIXME: re-add visibility check, along with properly adding p(ublish)time
-      $article = Article::findByCname( $this->objectId, $this->instanceId );
-      if ( $article->id() && $article->sectionId() == $this->instanceId )
+      switch( $dbArticle->type )
       {
-        $this->status = 200;
-        $this->title = $article->title();
-        $this->template = 'pages/articles/detail';
+        case 'Article':
+        case 'Kiki\Article':
+          $article->reset();
+          $article->setObjectId( $dbArticle->object_id );
+          $article->load();
+          // $article->setSectionId( $this->context );
 
-        $template = new Template( 'content/articles-single' );
-        $template->assign( 'article', $article->templateData() );
+          $template = new Template( 'content/articles-summary', true );
+          $template->assign( 'article', $article->templateData() );
 
-        $album = Album::findByLinkedObjectId( $article->objectId() );
-        $picture = new Picture( $album->getHighlightId() );
-        $storageItem = new StorageItem( $picture->storageId() );
-        $template->assign( 'image', $storageItem->url() );
+          $album = Album::findByLinkedObjectId( $article->objectId() );
+          $picture = new Picture( $album->getHighlightId() );
+          $storageItem = new StorageItem( $picture->storageId() );
+          $template->assign( 'image', $storageItem->url() );
 
-        $this->content = $template->fetch();
-      }
-      else
-      {
-        // $this->template = 'pages/default';
+          $this->content .= $template->fetch();
+          break;
 
-        // $template = new Template( 'content/articles-404' );
-        // $this->content = $template->fetch();
-
-        return false;
+        default:;
       }
     }
-    else
-    {
-      $section = new \Kiki\Section( $this->instanceId );
 
-      $itemsPerPage = 25;
-      if ( !isset($currentPage) )
-        $currentPage = 1;
-
-      $this->status = 200;
-      $this->title = $section->title();
-      $this->template = 'pages/articles/index';
-
-      $this->content = null; // MultiBanner::articles( $section->id() );
-
-      $article = new Article();
-
-      $q = $db->buildQuery( "SELECT count(*)
-        FROM articles a, objects o
-        WHERE o.object_id=a.object_id
-        AND type IN ('%s', '%s')
-          AND a.section_id=%d AND ((a.visible=1 AND ctime<=now()) OR user_id=%d)",
-        'Article', 'Kiki\Article',
-        $this->instanceId,
-        $user->id()
-      );
-      $totalPosts = $db->getSingleValue($q);
-
-      $paging = new \Kiki\Paging();
-      $paging->setCurrentPage( $currentPage );
-      $paging->setItemsPerPage( $itemsPerPage );
-      $paging->setTotalItems( $totalPosts );
-
-      $q = $db->buildQuery( "SELECT o.object_id, ctime, type
-        FROM articles a, objects o
-        WHERE o.object_id=a.object_id
-        AND type IN ('%s', '%s')
-        AND section_id=%d AND ( (a.visible=1 AND ctime<=now()) OR user_id=%d)
-        ORDER BY ctime DESC
-        LIMIT %d,%d",
-        'Article',
-        'Kiki\Article',
-        $this->instanceId,
-        $user->id(),
-        $paging->firstItem()-1,
-        $itemsPerPage
-      );
-      $articles = $db->getObjects($q);
-
-      foreach( $articles as $dbArticle )
-      {
-        switch( $dbArticle->type )
-        {
-          case 'Article':
-          case 'Kiki\Article':
-            $article->reset();
-            $article->setObjectId( $dbArticle->object_id );
-            $article->load();
-//            $article->setSectionId( $this->context );
-
-            $template = new Template( 'content/articles-summary', true );
-            $template->assign( 'article', $article->templateData() );
-
-            $album = Album::findByLinkedObjectId( $article->objectId() );
-            $picture = new Picture( $album->getHighlightId() );
-            $storageItem = new StorageItem( $picture->storageId() );
-            $template->assign( 'image', $storageItem->url() );
-
-            $this->content .= $template->fetch();
-            break;
-
-          default:;
-        }
-      }
-
-      $this->content .= $paging->html();
-    }
-
-    $this->content .= ob_get_contents();
-    ob_end_clean();
+    $this->content .= $paging->html();
 
     return true;
   }
 
+  public function detailAction()
+  {
+    $this->objectId = $this->action ?? null;
+
+    if ( !$this->context )
+      return false;
+
+    $this->initTemplateData();
+
+    $db = Core::getDb();
+    $user = Core::getUser();
+
+    $template = Template::getInstance();
+
+    $this->instanceId = Section::getIdFromBaseUri($this->context);
+
+/*
+    $q = $db->buildQuery( "SELECT id FROM articles a LEFT JOIN objects o ON o.object_id=a.object_id WHERE a.section_id=%d AND ((a.visible=1 AND o.ctime<=now()) OR o.user_id=%d) ORDER BY o.ctime DESC LIMIT 10", $this->instanceId, $user->id() );
+
+    $articleIds = $db->getObjectIds($q);
+    $articles = array();
+    foreach ( $articleIds as $articleId )
+    {
+      $article = new Article( $articleId );
+      $articles[] = array( 'url' => $article->url(), 'title' => $article->title() );
+    }
+    $template->assign( 'latestArticles', $articles );
+*/
+
+    // FIXME: re-add visibility check, along with properly adding p(ublish)time
+    $article = Article::findByCname( $this->objectId, $this->instanceId );
+    if ( !$article->id() )
+      return false;
+
+    $this->status = 200;
+    $this->title = $article->title();
+    $this->template = 'pages/articles/detail';
+
+    $template = new Template( 'content/articles-single' );
+    $template->assign( 'article', $article->templateData() );
+
+    $album = Album::findByLinkedObjectId( $article->objectId() );
+    $picture = new Picture( $album->getHighlightId() );
+    $storageItem = new StorageItem( $picture->storageId() );
+    $template->assign( 'image', $storageItem->url() );
+
+    $this->content = $template->fetch();
+
+    return true;
+  }
 }
--- templates/content/articles-single.tpl
+++ templates/content/articles-single.tpl
@@ -1,10 +1,10 @@
 <article id="article_{$article.id}">
   <header>
-    <h1>{$article.title}</h1>
+    <h1>{{$article.title|escape}}</h1>
     <time class="relTime" datetime="{$article.ctime|date:c}" pubdate>{$article.relTime} ago</time> &mdash;
     <span class="author">{$article.author}</span>
 {if $image}
-    <br><br><img src="{$image|thumb:800x300.c}" alt="[{$article.title|escape}]" class="rounded" />
+    <br><br><img src="{$image|thumb:800x300.c}" alt="{{$article.title|escape}}" class="rounded" />
 {/if}
   </header>
 
